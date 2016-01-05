f34144f983c30312d4a989013878e6ea54e9d594
taranir <shikonarella@gmail.com>
2016-01-05 12:51:04 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// Exercise 1
+// Change the font color of "Fruity website" to purple 
+// (TODO): YOUR CODE HERE
+
+// Exercise 2
+// Make the white div hide with an animation when you hover over it
+// and unhide when you stop hovering over it
+// (TODO): YOUR CODE HERE
+
+// Exercise 3
+// When the user writes text in the input box
+// and clicks the "GO!" button, display the text
+// in the white div
+// (TODO): YOUR CODE HERE
